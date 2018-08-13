0ffa0eb60fa63b8f2c1cfb1b6beeac190d9b4fb0
leonGravel <la9249@163.com>
2018-08-14 01:11:14 +0800

    修改目录结构，简化pom

--- ac-provider-coffee-macchiato/src/main/java/com/gravel/provider/Application.java
+++ ac-provider-coffee-macchiato/src/main/java/com/gravel/provider/Application.java
@@ -2,18 +2,10 @@ package com.gravel.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Application {
 
-	@Bean
-	@LoadBalanced
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
--- ac-provider-coffee-macchiato/src/main/java/com/gravel/provider/controller/IndexController.java
+++ ac-provider-coffee-macchiato/src/main/java/com/gravel/provider/controller/IndexController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * @author Gravel
