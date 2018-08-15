613aa7e411754a16a6af8fda7dc0fe48fdd13301
leonGravel <la9249@163.com>
2018-08-15 22:07:17 +0800

    :chart_with_upwards_trend: 添加配置中心

---
+++ ac-config/src/main/java/com/gravel/config/AcConfigApplication.java
@@ -0,0 +1,12 @@
+package com.gravel.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcConfigApplication.class, args);
+	}
+}
